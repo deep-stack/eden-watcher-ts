88ba3ba24c0b7d698d53c02ab2e7964c9519cfeb
nikugogoi <95nikass@gmail.com>
2023-04-13 08:06:45 +0530

    Copy over watcher from watcher-ts

---
+++ src/entity/Slash.ts
@@ -0,0 +1,32 @@
+//
+// Copyright 2021 Vulcanize, Inc.
+//
+
+import { Entity, PrimaryColumn, Column, Index } from 'typeorm';
+
+import { bigintTransformer } from '@cerc-io/util';
+
+@Entity()
+@Index(['blockNumber'])
+export class Slash {
+  @PrimaryColumn('varchar')
+    id!: string;
+
+  @PrimaryColumn('varchar', { length: 66 })
+    blockHash!: string;
+
+  @Column('integer')
+    blockNumber!: number;
+
+  @Column('numeric', { transformer: bigintTransformer })
+    timestamp!: bigint;
+
+  @Column('varchar')
+    account!: string;
+
+  @Column('numeric', { transformer: bigintTransformer })
+    slashed!: bigint;
+
+  @Column('boolean', { default: false })
+    isPruned!: boolean;
+}
